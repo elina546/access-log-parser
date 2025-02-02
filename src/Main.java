947c55210c748ae1433cbc0b8d95163a96f296dd
Elina Derlyuk <elina.bast54@gmail.com>
2025-02-02 12:57:16 +0300

    modification Main class added file path check for parsing

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,32 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int n = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println(" Please enter the path:");
+            String path = scanner.nextLine();
+            File file = new File(path);
+
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            // Debugging: Print the values of fileExists and isDirectory
+            System.out.println("File exists: " + fileExists);
+            System.out.println("Is directory: " + isDirectory);
+
+            if (!fileExists || isDirectory)  {
+                System.out.println("File doesn't exist or path is incorrect");
+                continue;
+            } else {
+                n++;
+                System.out.println("Path is correct" + '\n' + "File number is " + n);
+            }
+        }
     }
 }
+
+
+
